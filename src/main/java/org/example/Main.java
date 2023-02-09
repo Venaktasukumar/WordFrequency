6f8f0773fcc952c99a8329880b46070c947eb221
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-02-09 22:45:03 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+class Frequency{
+    public static void main(String[] args) throws FileNotFoundException{
+        Logger l= Logger.getLogger("com.api.jar");
+        File file=new File("C:/Users/Tringapps-User12/Documents/frequencyfile/text.txt");
+        Scanner sc=new Scanner(file);
+        HashMap<String,Integer> hash=new HashMap<String,Integer>();
+        Queue<Map.Entry<String,Integer>> queue=new PriorityQueue<>((a,b)->{return b.getValue()-a.getValue();});
+        while(sc.hasNext()){
+            String word=sc.next();
+            if(hash.containsKey(word)){
+                hash.put(word,hash.get(word)+1);
+            }
+            else{
+                hash.put(word,1);
+            }
+        }
+        l.log(Level.INFO,()->""+hash);
+        queue.addAll(hash.entrySet());
+        for(Map.Entry<String,Integer> q:queue){
+            String k=q.getKey()+"->"+q.getValue();
+            l.info(k);
+        }
+
+    }
+}
