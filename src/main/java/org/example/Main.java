a484337a25bd10b1090c945df49f331a67214b30
Venaktasukumar <malleboina.sukumar@tringapps.com>
2023-02-10 10:46:25 +0530

    COMMIT

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ class Frequency{
     public static void main(String[] args) throws FileNotFoundException{
         Logger l= Logger.getLogger("com.api.jar");
         String s="C:/Users/Tringapps-User12/Documents/frequencyfile/text.txt";
-        File file=new File(s);
-        Scanner sc=new Scanner(file);
+        File fi=new File(s);
+        Scanner sc=new Scanner(fi);
         HashMap<String,Integer> hash=new HashMap<String,Integer>();
         while(sc.hasNext()){
             String word=sc.next();
@@ -21,9 +21,13 @@ class Frequency{
         }
         l.info("Before Sorting:");
         l.log(Level.INFO,()->""+hash);
-        List<Map.Entry<String, Integer>> k= hash.entrySet().stream().sorted(Collections.reverseOrder(Map.Entry.comparingByValue())).toList();
         l.info("After Sorting:");
+        ArrayList<Map.Entry<String,Integer>> k=new ArrayList<>(hash.entrySet());
+        Collections.sort(k,(a,b)->b.getValue().compareTo(a.getValue()));
         l.log(Level.INFO,()->""+k);
-
+        for(Map.Entry<String,Integer> i:k){
+            String f= i.getKey()+" -> "+i.getValue();
+            l.info(f);
+        }
     }
 }
